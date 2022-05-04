8e649f14666951bf26d6ca1d1d92eb8b14fc6186
wiamRB <101800097+wiamRB@users.noreply.github.com>
2022-05-04 15:54:32 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-
+import xmlrpc.client
 import datetime
 class test:
   def __init__(self,date,nom,prenom):
@@ -11,3 +11,9 @@ nom=input("nom: \n")
 date=datetime.datetime.now().time()
 donnee=test(date,nom,prenom)
 print(donnee.date,donnee.nom,donnee.prenom)
+proxy=xmlrpc.client.ServerProxy("http://192.168.56.102:6789/")
+result=proxy.nbre(donnee)
+print(f"result:{result}")
+proxy=xmlrpc.client.ServerProxy("http://192.168.56.102:6789/")
+result=proxy.nbre(donnee)
+print(f"result:{result}")
