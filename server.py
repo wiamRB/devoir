bbb5a675b8e510448557fa7618c5084b2f51e13d
wiamRB <101800097+wiamRB@users.noreply.github.com>
2022-05-04 15:50:45 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,10 @@
 
+from xmlrpc.server import SimpleXMLRPCServer
+def enr(objet):
+    f = open('file.txt', 'r+')
+    f.write(object) 
+    return("ok")
+
+server=Simple XMLRPCServer(("0.0.0.0",6789))
+server.register_function(enr,"enr")
+server.serve_forever()
