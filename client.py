3860e399abef68191e38e79981792316a35ae9db
wiamRB <101800097+wiamRB@users.noreply.github.com>
2022-05-04 15:41:36 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,13 @@
 
+import datetime
+class test:
+  def __init__(self,date,nom,prenom):
+      self.date=date
+      self.prenom=prenom
+      self.nom=nom
+   
+prenom=input("prenom: \n")
+nom=input("nom: \n")
+date=datetime.datetime.now().time()
+donnee=test(date,nom,prenom)
+print(donnee.date,donnee.nom,donnee.prenom)
